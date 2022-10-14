b67ff94085ac1536372de7f76747f204bd541ec0
YolandaRR <yolanda.ruiz.rebollo@emeal.nttdata.com>
2022-10-14 10:13:05 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+import './styles.css';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
